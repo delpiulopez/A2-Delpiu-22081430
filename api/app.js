e5f058c111c3c82a7ff0f57f65acac54833ddfc8
Delpiu <delpiulopez@gmail.com>
2024-12-10 11:41:01 +0700

    Implemented the routes in the app.js

--- api/app.js
+++ api/app.js
@@ -29,6 +29,9 @@ require("./routes/companies.routes")(app);
 require("./routes/contacts.routes")(app);
 require("./routes/phones.routes")(app);
 require("./routes/stats.routes")(app);
+require("./routes/orders.routes")(app);
+require("./routes/items.routes")(app);
+require("./routes/customers.routes")(app);
 
 // set port, listen for requests
 const PORT = 5000;
